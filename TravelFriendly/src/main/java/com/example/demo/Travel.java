bf80b326298132a0ddcbb0fa91bb570562ec5fa3
reinachikita <violetatrujilloalmeida@gmail.com>
2023-02-02 12:41:49 +0100

    Update Travel.java
    j

--- TravelFriendly/src/main/java/com/example/demo/Travel.java
+++ TravelFriendly/src/main/java/com/example/demo/Travel.java
@@ -1,10 +1,6 @@
 package com.example.demo;
 import java.util.Date;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 //@Entity
 public class Travel{
 	//@Id
