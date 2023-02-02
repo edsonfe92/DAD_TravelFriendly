ac3cc88ed2a8224c79f4914aef36a8c4812a0492
reinachikita <violetatrujilloalmeida@gmail.com>
2023-02-02 12:39:57 +0100

    travel
    travel

---
+++ TravelFriendly/src/main/java/com/example/demo/Travel.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+//@Entity
+public class Travel{
+	//@Id
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String origen;
+	private String destino;
+	private String conductor;
+	private String[] pasajeros;
+	private Date fecha;
+	
+	public Travel() {
+		
+	}
+	
+	public Travel(String o, String d, String c, String[] p, Date f) {
+		super();
+		origen=o;
+		destino=d;
+		for(int i=0;i<p.length;i++) {
+			pasajeros[i]=p[i];
+		}
+		fecha=f;
+	}
+	
+	//@RestController
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public String getOrigen() {
+		return origen;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public String getConductor() {
+		return conductor;
+	}
+	public String[] getPasajeros() {
+		return pasajeros;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	
+	public void setId(Long id) {
+		this.id=id;
+	}
+	public void setOrigen(String origen) {
+		this.origen=origen;
+	}
+	public void setDestino(String destino) {
+		this.destino=destino;
+	}
+	public void setConductor(String conductor) {
+		this.conductor=conductor;
+	}
+	public void setPasajeros(String[] pasajeros) {
+		this.pasajeros=pasajeros;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha=fecha;
+	}
+	
+	
+	
+	
+}
